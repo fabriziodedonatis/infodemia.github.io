372a5916ac8853c0998ec1aa6cb054d087013bbf
barsab <b.sabato@unirsm.sm>
2020-06-06 16:45:30 +0200

    Inserimento bubble finali
    Bubble box_11 e box_12 codice corretto

--- file sito/js/bubble-finali.js
+++ file sito/js/bubble-finali.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-         var words=["mascherine","psicosi","sanificare","multa","peste","quarantena","decreto","coronavirus","bloccare","allerta","chiusura","emergenza","morti","pandemia","panico","untori","paura"];
+         /*var words=["mascherine","psicosi","sanificare","multa","peste","quarantena","decreto","coronavirus","bloccare","allerta","chiusura","emergenza","morti","pandemia","panico","untori","paura"];
          for(n=0; n<10; n++){
          for (i = 0; i < words.length; i++) {
             tmpBubble=$(".bubbles").append("<p class='bubble'>"+ words[i] + "</p>");
@@ -11,7 +11,7 @@ $(document).ready(function() {
             });
            });
          }
-         }
+         }*/
          var controller = new ScrollMagic.Controller();
          new ScrollMagic.Scene({
                     triggerElement: "#box_bubble",
@@ -25,14 +25,20 @@ $(document).ready(function() {
                 .on("enter", function(e) {
                 $(".bubbles .bubble").each(function() {
                 rndScale = Math.random() * 3;
-                $(this).css("opacity", "1");
+                rndX = Math.random() * $(".bubbles").width();
+                rndY = Math.random() * $(".bubbles").height();
+                rndOrbit = Math.random() * 5;
+                speed = Math.random() * 2;
                 $(this).css("transform", "scale(" + rndScale + ")");
+                $(this).css("top", rndY + "px");
+                $(this).css("left", rndX + "px");
+                $(this).css("opacity", "1");
             })
                 })
                 .setClassToggle(".bubbles", "bg")
                 .addTo(controller);
                 
-                new ScrollMagic.Scene({ triggerElement: "#box_12",	duration: "100%", offset: "50",triggerHook: 0.5, })
+                new ScrollMagic.Scene({ triggerElement: "#box_12",	duration: "50%", offset: "50",triggerHook: 0.5, })
 				.setPin("#box_12", {	pushFollowers: false})
 				.addIndicators() // add indicators (requires plugin)
 				.on("enter", function(e) {
